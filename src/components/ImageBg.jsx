0bcd40e08e5a1a25cd25f14470e9e7ee5828d4c0
Rasmus Olsen <rasmusolsen@Rasmuss-MBP.home>
2024-10-29 19:43:50 +0100

    færdig

--- src/components/ImageBg.jsx
+++ src/components/ImageBg.jsx
@@ -7,11 +7,11 @@ const ImageBg = () => {
         <Image className="h-24 object-contain z-10" src="/navy.png" width={500} height={500} alt="Iwatch 1" />
         <div className="absolute bottom-0 w-full h-12 bg-[#434558] bg-opacity-60 rounded-lg"></div>
       </div>
-      <div className="relative w-32  flex flex-col items-center cursor-pointer">
+      <div className="relative flex flex-col items-center cursor-pointer">
         <Image className="h-24 object-contain z-10" src="/mint.png" width={500} height={500} alt="Iwatch 2" />
         <div className="absolute bottom-0 w-full h-12 bg-[#6ADDCC]  bg-opacity-60 rounded-lg"></div>
       </div>
-      <div className="relative w-32  flex flex-col items-center cursor-pointer">
+      <div className="relative flex flex-col items-center cursor-pointer">
         <Image className="h-24 object-contain z-10" src="/ocean.png" width={500} height={500} alt="Iwatch 3" />
         <div className="absolute bottom-0 w-full h-12 bg-[#F9CDC4]  bg-opacity-60 rounded-lg"></div>
       </div>
