51bf6f8dc46d4d106d0202458b517e50d3a9c6ec
Rasmus Olsen <rasmusolsen@Rasmuss-MacBook-Pro.local>
2024-11-04 14:48:24 +0100

    click på billederne skifter

--- src/components/ImageBg.jsx
+++ src/components/ImageBg.jsx
@@ -1,20 +1,10 @@
 import Image from "next/image";
 
-const ImageBg = () => {
+const ImageBg = ({ onClick, bgColor, imageSkift }) => {
   return (
-    <div className="colors grid grid-cols-3 gap-10">
-      <div className="relative flex flex-col items-center cursor-pointer">
-        <Image className="h-24 object-contain z-10" src="/navy.png" width={500} height={500} alt="Iwatch 1" />
-        <div className="absolute bottom-0 w-full h-14 bg-[#434558] bg-opacity-60 rounded-lg"></div>
-      </div>
-      <div className="relative flex flex-col items-center cursor-pointer">
-        <Image className="h-24 object-contain z-10" src="/mint.png" width={500} height={500} alt="Iwatch 2" />
-        <div className="absolute bottom-0 w-full h-14 bg-[#6ADDCC]  bg-opacity-60 rounded-lg"></div>
-      </div>
-      <div className="relative flex flex-col items-center cursor-pointer">
-        <Image className="h-24 object-contain z-10" src="/ocean.png" width={500} height={500} alt="Iwatch 3" />
-        <div className="absolute bottom-0 w-full h-14 bg-[#F9CDC4]  bg-opacity-60 rounded-lg"></div>
-      </div>
+    <div onClick={onClick} className="relative flex flex-col items-center cursor-pointer w-36 h-fit">
+      <Image src={imageSkift} alt="Small view of watch option" className="h-24 object-contain z-10" />
+      <div className={`absolute bottom-0 w-full h-[60%] ${bgColor} rounded-lg`}></div>
     </div>
   );
 };
--- src/components/TextImage.jsx
+++ src/components/TextImage.jsx
@@ -1,8 +1,19 @@
-import Image from "next/image";
+"use client"; // Sørg for at køre på client-side i Next.js
 
+import { useState } from "react";
+import Image from "next/image";
 import ImageBg from "./ImageBg";
 
+import SortImg from "../../public/navy.png"; // Standard billede
+import TealImg from "../../public/mint.png";
+import PinkImg from "../../public/ocean.png";
+
 const TextImage = (props) => {
+  const [imgSrc, setImgSrc] = useState(SortImg); // Holder styr på det store billede
+
+  const handleImageChange = (newImage) => {
+    setImgSrc(newImage); // Opdaterer det store billede, når et lille billede klikkes
+  };
   return (
     <section className="grid grid-cols-[50%_48%_2%] max-w-7xl mx-auto mt-20 mb-20 h-full">
       <div className="grid gap-2 pt-20">
@@ -17,9 +28,16 @@ const TextImage = (props) => {
           <Image className="my-0 mx-auto cursor-pointer" src="/pil-højre.svg" width={18} height={2} alt="arrow" />
         </div>
       </div>
-      <div className="images">
-        <Image className="mb-10" src="/navy.png" width={500} height={500} alt="Iwatch" />
-        <ImageBg />
+
+      <div className="grid gap-6 justify-center">
+        <div className="grid items-center justify-center">
+          <Image src={imgSrc} alt="Apple watch" width={500} height={500} />
+        </div>
+        <div className="flex justify-between w-full">
+          <ImageBg bgColor="bg-[#434558]" imageSkift={SortImg} onClick={() => handleImageChange(SortImg)} />
+          <ImageBg bgColor="bg-[#6ADDCC]" imageSkift={TealImg} onClick={() => handleImageChange(TealImg)} />
+          <ImageBg bgColor="bg-[#F9CDC4]" imageSkift={PinkImg} onClick={() => handleImageChange(PinkImg)} />
+        </div>
       </div>
       <div className="flex flex-col items-center mt-auto mb-auto">
         <div className="w-5 h-5 bg-[#434558] rounded-full border-2 border-white transform transition-transform duration-300 hover:scale-150 cursor-pointer"></div>
